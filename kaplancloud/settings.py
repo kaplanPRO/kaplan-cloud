5bb081b4d2346976c050581b993f3adb7a205e08
Çağatay Onur Şengör <contact@csengor.com>
2021-11-09 11:10:14 +0300

    Randomize default SECRET_KEY and look for it in env variables

--- kaplancloud/settings.py
+++ kaplancloud/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import random
+import string
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yc)89-h47vor83$qvs+)x6telrn!pnug*c5_-a5yr!lhux@^*l'
+SECRET_KEY = ''.join(os.environ.get('SECRET_KEY', random.choices(string.printable, k=62)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
