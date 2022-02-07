70d4f042ab7e6b09454304f68fbc8b166212b59d
Çağatay Onur Şengör <cagatay@kaplan.pro>
2022-02-07 21:48:05 +0300

    :wrench: Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -z "${SECRET_KEY}" ]]; then
+  echo "SECRET_KEY is unset. Generating it"
+  export SECRET_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9&-@' | fold -w 62 | head -n 1)
+fi
+
 # Collect static files
 echo "Collect static files"
 python manage.py collectstatic --noinput
@@ -14,8 +19,8 @@ python manage.py loaddata pm-group
 
 # Start server
 echo "Starting server"
-if [[ -z "${USE_GUNICORN}" ]]; then
-  python manage.py runserver 0.0.0.0:${VIRTUAL_PORT-8080}
+if [[ "${USE_GUNICORN:-False}" = "True" ]]; then
+  gunicorn kaplancloud.wsgi:application -b 0.0.0.0:8080 -w ${GUNICORN_WORKERS:-2} -t ${GUNICORN_TIMEOUT:-300}
 else
-  gunicorn kaplancloud.wsgi:application -b 0.0.0.0:${VIRTUAL_PORT-8080} -w ${GUNICORN_WORKERS-2} -t ${GUNICORN_TIMEOUT-300}
+  python manage.py runserver 0.0.0.0:8080
 fi
