c26316e7527f0545afb2fa0b8b8fe9f700406e19
Çağatay Onur Şengör <cagatay@kaplan.pro>
2022-03-17 18:41:29 +0300

    :bookmark: version 0.3.0-rc.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Kaplan'
 author = 'Kaplan'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.2'
+release = '0.3.0-rc.1'
 
 
 # -- General configuration ---------------------------------------------------
