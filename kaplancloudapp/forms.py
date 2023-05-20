107cba909f6b8d28248b6f75947c7ca04b35377a
Çağatay Onur Şengör <cagatay@kaplan.pro>
2023-05-20 12:03:44 +0300

    :arrow_up: Upgrade Django to 4.2.x

--- kaplancloudapp/forms.py
+++ kaplancloudapp/forms.py
@@ -41,8 +41,8 @@ class ProjectForm(forms.Form):
     target_language = forms.ModelChoiceField(queryset=LanguageProfile.objects.all(), to_field_name='iso_code', help_text='If you don\'t see the language you need, please create a LanguageProfile in the Admin dashboard.')
     translation_memories = forms.ChoiceField(required=False)
     client = forms.ModelChoiceField(queryset=Client.objects.all(), required=False)
-    project_files = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True, 'accept': ','.join(['.docx', '.json', '.kxliff', '.odp', '.ods', '.odt', '.po', '.sdlxliff', '.txt', '.xliff'])}))
-    reference_files = forms.FileField(required=False, widget=forms.ClearableFileInput(attrs={'multiple':True}))
+    project_files = forms.FileField(widget=forms.ClearableFileInput(attrs={'allow_multiple_selected': True, 'accept': ','.join(['.docx', '.json', '.kxliff', '.odp', '.ods', '.odt', '.po', '.sdlxliff', '.txt', '.xliff'])}))
+    reference_files = forms.FileField(required=False, widget=forms.ClearableFileInput(attrs={'allow_multiple_selected':True}))
     due_by = forms.DateTimeField(required=False, widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
     will_pretranslate = forms.BooleanField(label='Pretranslate from TM', required=False)
 
